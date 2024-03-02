51316ee12ea0ba30dbe0ef233ecd79937dca21ab
Dilshad2020 <161384835+Dilshad2020@users.noreply.github.com>
2024-03-03 01:01:23 +0400

    Add files via upload

---
+++ cüt tək ədədlər.py
@@ -0,0 +1,6 @@
+print("ədədlərin tək və cüt olduğunu yoxlayan sistemə xoş gəlmisiniz! ")
+ədəd=int(input("zəhmət olmasa ədədi qeyd edin: "))
+if ədəd%2==0:
+    print("ədəd cütdür və 2-yə bölünür: ")
+else:
+    print("ədəd təkdir və 2-yə bölünmür: ")
