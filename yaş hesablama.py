7588d58ab906ed8cb38cec90742f5eead8d1ca6d
Dilshad2020 <161384835+Dilshad2020@users.noreply.github.com>
2024-03-03 00:55:36 +0400

    Add files via upload

---
+++ yaş hesablama.py
@@ -0,0 +1,7 @@
+print("Salam yaş hesablama sisteminə xoş gəlmisiniz.")
+ad=input("Zəhmət olmasa adınızı qeyd edin: ")
+yaş=input("Zəhmət olmasa doğulduğunuz ili qeyd edin: ")
+yaş1=2024-int(yaş)
+YH=yaş1*52
+print("zəhmət olmasa gözləyin.......")
+print(f" {ad}  {yaş}  {yaş1} yaşın var \n{YH} həftə yaşamısan" )
